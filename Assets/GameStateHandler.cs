d8e924f1ccd4e1ee4ae2087acd8e9f10dc370fd4
PJWoods <patrik.skoog83@gmail.com>
2016-02-15 20:04:20 +0100

    push/pop
    gg

--- Assets/GameStateHandler.cs
+++ Assets/GameStateHandler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public abstract class GameState
 {
@@ -14,16 +15,35 @@ public class GameStateHandler
 {
 	static public bool EditorMode = false;
 	private GameState m_currentState;
+	private List<GameState> m_states;
 
     public void Initialize()
     {
         SetState(new MenuState());
-       
     }
     public void ChangeState(GameState state)
     {
         SetState(state);
     }
+	public void Push(GameState state)
+	{
+		state.Begin();
+		m_states.Insert(0, state);
+		m_currentState = state;
+	}
+	public void Pop()
+	{
+		if(m_states.Count > 0)
+		{
+			m_states[0].End();
+			m_states.RemoveAt(0);
+
+			if(m_states.Count > 0)
+				m_currentState = m_states[0];
+			else
+				m_currentState = null;
+		}
+	}
 	public GameState GetCurrentState()
 	{
 		return m_currentState;
@@ -34,7 +54,6 @@ public class GameStateHandler
             m_currentState.End();
 
         m_currentState = state;
-
         m_currentState.Begin();
     }
 }
